eee96b83d29804244502140260c9fa66d652c81c
FreakJazz <jazzrb2307@hotmail.com>
2020-07-08 20:02:17 -0500

    Prube steps calibration

--- program/program.ino
+++ program/program.ino
@@ -6,7 +6,7 @@
 LiquidCrystal lcd(0, 1, 4, 5, 6, 7);
 
 // change this to the number of steps on your motor
-#define STEPS 150
+#define STEPS 100
 
 // create an instance of the stepper class, specifying
 // the number of steps of the motor and the pins it's
