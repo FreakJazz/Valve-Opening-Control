da1075c918d57889776a45698c8cb3a8686d2f4e
FreakJazz <jazzrb2307@hotmail.com>
2020-07-08 19:59:41 -0500

    Arduino Programming

--- program/program.ino
+++ program/program.ino
@@ -1,35 +1,8 @@
-/*
- * MotorKnob
- *
- * A stepper motor follows the turns of a potentiometer
- * (or other sensor) on analog input 0.
- *
- 
- The circuit:
- * LCD RS pin to digital pin 0
- * LCD Enable pin to digital pin 1
- * LCD D4 pin to digital pin 4
- * LCD D5 pin to digital pin 5
- * LCD D6 pin to digital pin 6
- * LCD D7 pin to digital pin 7
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
- 
- 
- 
- 
- * http://www.arduino.cc/en/Reference/Stepper
- * This example code is in the public domain.
- */
 
 #include <Stepper.h>
 #include <LiquidCrystal.h>
 
-// initialize the library with the numbers of the interface pins
+// initialize the library with the numbers of the LCD pins
 LiquidCrystal lcd(0, 1, 4, 5, 6, 7);
 
 // change this to the number of steps on your motor
