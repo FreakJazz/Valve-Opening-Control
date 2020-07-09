b7e27761d37e7b244d5fc3cbbeae890c0671ac44
FreakJazz <jazzrb2307@hotmail.com>
2020-07-08 20:01:13 -0500

    Prube steps in the valve

--- program/program.ino
+++ program/program.ino
@@ -6,7 +6,7 @@
 LiquidCrystal lcd(0, 1, 4, 5, 6, 7);
 
 // change this to the number of steps on your motor
-#define STEPS 100
+#define STEPS 150
 
 // create an instance of the stepper class, specifying
 // the number of steps of the motor and the pins it's
